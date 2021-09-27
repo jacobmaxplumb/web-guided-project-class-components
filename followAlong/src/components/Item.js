3cddd84a562ca795dbac56787ed9b015029361b4
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-27 13:31:37 -0400

    lecture work-in-progress

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Item = props => {
+  const handleClick = ()=> {
+    console.log("Clicked item");
+  }
+
   return (
-    <div className={`item${props.item.purchased ? ' purchased' : ''}`}>
+    <div onClick={handleClick}className={`item${props.item.purchased ? ' purchased' : ''}`}>
       <p>{props.item.name}</p>
     </div>
   );
