39def683fa001b13a60d56e778b1e147c19f6b56
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-27 12:22:49 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const App = () => {
-  const name = "Allison";
+  const name = "Chris";
   
   return (
     <div>
-      <h1>Hello Warren</h1>
+      <h1>Hello {name}</h1>
     </div>
   );
 };
