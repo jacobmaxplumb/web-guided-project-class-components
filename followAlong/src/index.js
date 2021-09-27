7977d63c778c5be9bc330a2bdcac6a4aa56f7bc7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-27 16:46:19 -0400

    lecture work-in-progress

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -98,7 +98,7 @@ class App extends React.Component {
       <div className="App">
         <div className="header">
            <h1>Shopping List</h1>
-           <ListForm handleAddItem={this.handleAddItem} setState={this.setState}/>
+           <ListForm handleAddItem={this.handleAddItem} setState={this.setState} groceries={this.state.groceries}/>
          </div>
         <GroceryList handleClear={this.handleClear} handleToggle={this.handleToggle} groceries={this.state.groceries} />
        </div>
