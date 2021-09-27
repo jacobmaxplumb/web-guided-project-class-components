c855e563faa3b47b9db2886d52237335af47c19b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-27 13:42:47 -0400

    lecture work-in-progress

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
 
 const Item = props => {
-  const [ item, setItem ] = useState({
-    name: 'Bananas',
-    id: 123,
-    purchased: false
-  });
+  // const [ item, setItem ] = useState({
+  //   name: 'Bananas',
+  //   id: 123,
+  //   purchased: false
+  // });
 
   const handleClick = ()=> {
     // console.log("Clicked item ", item.name);
     //How do we set purchased to true?
-    setItem({
-      ...item,
-      purchased: !item.purchased
-    });
+
+    // setItem({
+    //   ...item,
+    //   purchased: !item.purchased
+    // });
+    props.handleToggle();
   }
 
   console.log(props);
-  
+
   return (
-    <div onClick={handleClick}className={`item${item.purchased ? ' purchased' : ''}`}>
-      <p>{item.name}</p>
+    <div onClick={handleClick} className={`item${props.item.purchased ? ' purchased' : ''}`}>
+      <p>{props.item.name}</p>
     </div>
   );
 };
