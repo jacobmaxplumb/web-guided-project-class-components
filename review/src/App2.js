14f7c3f8315bdbd9ef0445a6083943c337fcaaa3
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-27 13:08:15 -0400

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Person = (props)=> {
     return(<div>
         <h1>Hello {props.name}. You are {props.age} years old.</h1>
-        <p>{props.name} is a great name?</p>
+        <p>{props.name} is a great name!</p>
     </div>);
 }
 
@@ -27,7 +27,7 @@ class App2 extends React.Component {
 
     render() {
         return(<div>
-            <Person name={this.name} age={this.age}/>
+            <Person name={this.state.name} age={this.state.age}/>
             <button onClick={this.handleClick}>Change Name</button>
         </div>);
     }
